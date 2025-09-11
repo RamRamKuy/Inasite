1ce007b18b0c954b25ae1aff366b41b6814e0052
RamRam <ramadhandafa@apps.ipb.ac.id>
2025-09-11 18:06:12 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,6 +7,9 @@ const footer_input = document.querySelector(".footer-input");
 const hamburger_menu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector("header nav");
 const links = document.querySelectorAll(".links a");
+const footer_input = document.querySelector(".footer-input"); // input di footer
+const navbar = document.querySelector(".navbar");             // navbar utama
+const hamburger_menu = document.querySelector(".hamburger-menu"); // tombol hamburger
 
 footer_input.addEventListener("focus", () => {
   footer_input.classList.add("focus");
