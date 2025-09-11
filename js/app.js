552905dd9be2c280ca1df43ff5a7bfca06f05bcb
RamRam <ramadhandafa@apps.ipb.ac.id>
2025-09-11 18:10:37 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,22 +3,9 @@ const skills_wrap = document.querySelector(".skills");
 const skills_bars = document.querySelectorAll(".skill-progress");
 const records_wrap = document.querySelector(".records");
 const records_numbers = document.querySelectorAll(".number");
-const footer_input = document.querySelector(".footer-input");
 const hamburger_menu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector("header nav");
 const links = document.querySelectorAll(".links a");
-const footer_input = document.querySelector(".footer-input"); // input di footer
-const navbar = document.querySelector(".navbar");             // navbar utama
-const hamburger_menu = document.querySelector(".hamburger-menu"); // tombol hamburger
-
-footer_input.addEventListener("focus", () => {
-  footer_input.classList.add("focus");
-});
-
-footer_input.addEventListener("blur", () => {
-  if (footer_input.value != "") return;
-  footer_input.classList.remove("focus");
-});
 
 function closeMenu() {
   navbar.classList.remove("open");
